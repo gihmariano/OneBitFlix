571a1d2390c8f8944a47f6e20f8a24996aa84ccd
gihmariano <giane.pedro@fatec.sp.gov.br>
2018-11-19 16:56:42 -0200

    alteração rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "home#index"
-  
+
   namespace :api do
    namespace :v1 do
     get '/dashboard', to: 'dashboards#index', as: 'dashboard'
