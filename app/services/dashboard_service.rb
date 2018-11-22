66831160a3b39e6aab25b57ae2c795abc622fb75
gihmariano <giane.pedro@fatec.sp.gov.br>
2018-11-22 12:46:41 -0200

    dashboard service

--- app/services/dashboard_service.rb
+++ app/services/dashboard_service.rb
@@ -17,15 +17,13 @@ class DashboardService
   end
 
   def group_by_keep_watching
-   players = Player.includes(:movie).where(end_date: nil, user:
-@user)
+   players = Player.includes(:movie).where(end_date: nil, user:@user)
    Api::V1::MovieSerializer.new(players.map(&:movie))
   end
 
   def group_by_highlight
    highlight = Movie.find_by(highlighted: true)
    highlight ||= Serie.find_by(highlighted: true)
-   Api::V1::WatchableSerializer.new(highlight, params: { user:
-@user })
+   Api::V1::WatchableSerializer.new(highlight, params: { user:@user })
   end
 end
