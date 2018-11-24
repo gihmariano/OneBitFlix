5ccac61c7c7f68c63233064807c7ef23a54f6176
gihmariano <giane.pedro@fatec.sp.gov.br>
2018-11-24 00:52:38 -0200

    ajustando serializer

--- app/serializers/api/v1/watchable_serializer.rb
+++ app/serializers/api/v1/watchable_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::WatchableSerializer
  include FastJsonapi::ObjectSerializer
- attributes :id, :title, :description, :thumbnail_key, :featured_thumbnail_key
+ attributes :id, :title, :description
 
  attribute :type do |object|
   object.model_name
@@ -9,16 +9,34 @@ class Api::V1::WatchableSerializer
  attribute :favorite do |object, params|
   if params.present? && params.has_key?(:user)
    params[:user].favorites.where(favoritable: object).exists?
-   end
- end
-
- attribute :video_key do |object|
-  if object[:video_key].present?object.video_key
   end
  end
 
- attribute :featured_thumbnail_key do |object|
-  if object[:featured_thumbnail_key].present?object.featured_thumbnail_key
+ attribute :thumbnail_url do |object|
+
+AWS_BUCKET.object("thumbnails/#{object.thumbnail_key}").presigned_url
+(:get, expires_in: 120)
+ end
+
+ attribute :thumbnail_cover_url do |object|
+
+AWS_BUCKET.object("thumbnails/#{object.thumbnail_cover_key}").presign
+ed_url(:get, expires_in: 120)
+ end
+
+ attribute :video_url do |object|
+  if object[:video_key].present?
+
+AWS_BUCKET.object("videos/#{object.video_key}").presigned_url(:get,
+expires_in: 120)
+  end
+ end
+
+ attribute :featured_thumbnail_url do |object|
+  if object[:featured_thumbnail_key].present?
+
+AWS_BUCKET.object("thumbnails/#{object.featured_thumbnail_key}").pres
+igned_url(:get, expires_in: 120)
   end
  end
 end
